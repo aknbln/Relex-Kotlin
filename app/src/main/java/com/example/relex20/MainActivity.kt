f55ad9dfc195a7a9ee7d4cb8f6503c16346e9755
aknbln <73264040+aknbln@users.noreply.github.com>
2022-10-23 14:25:04 -0500

    bottom navigation bar started

--- app/src/main/java/com/example/relex20/MainActivity.kt
+++ app/src/main/java/com/example/relex20/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.relex20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
