24bc8eb509031c220e099082c73a01b29944b78b
Spaceman213 <73033227+Spaceman213@users.noreply.github.com>
2022-10-26 11:26:24 -0500

    Fragment navigation

--- app/src/main/java/com/example/relex20/MainActivity.kt
+++ app/src/main/java/com/example/relex20/MainActivity.kt
@@ -12,7 +12,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        loadFragment(MapsFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.maps -> {
+                    loadFragment(MapsFragment())
+                    true
+                }
+                R.id.scan -> {
+                    loadFragment(ScanFragment())
+                    true
+                }
+                R.id.account -> {
+                    loadFragment(AccountFragment())
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
     }
 
     private fun loadFragment(fragment: Fragment){
